e274ee29ca66ba29973e7d162049477d4ffb5fa9
Giorgi Zviadadze <giosofteng@gmail.com>
2024-08-14 19:20:11 -0400

    Implement boards page design

---
+++ src/pages/BoardListPage/BoardItem.jsx
@@ -0,0 +1,27 @@
+const BOARD_ITEMS_INFO = [
+  { desc: "Flowers", price: 44.95 },
+  { desc: "Cartoon Mascot", price: 39.95 },
+  { desc: "Zombie Bride", price: 64.95 },
+  { desc: "Classic Guitar", price: 79.95 },
+  { desc: "Spaceships", price: 39.95 },
+  { desc: "Exotic Car", price: 64.95 },
+  { desc: "Shapes and Colors", price: 44.95 },
+  { desc: "Urban Landscape", price: 79.95 },
+  { desc: "Pixel Art", price: 44.95 },
+];
+
+export const BoardItem = ({ index }) => (
+  <div className="flex flex-col w-64 m-4 font-body text-neutral-600 shadow-md shadow-neutral-200">
+    <div className="relative">
+      <img
+        src={`/grb/board_${index}.webp`}
+        alt={`Board with ${BOARD_ITEMS_INFO[index].desc} design.`}
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-white/10 hover:bg-orange-400/50"></div>
+    </div>
+    <div className="flex justify-between p-2">
+      <div>{BOARD_ITEMS_INFO[index].desc}</div>
+      <div>{`$${BOARD_ITEMS_INFO[index].price}`}</div>
+    </div>
+  </div>
+);
--- src/pages/BoardListPage/index.jsx
+++ src/pages/BoardListPage/index.jsx
@@ -1,7 +1,17 @@
 import { NavBar } from "shared-components/NavBar";
+import { BoardItem } from "./BoardItem";
 
 export const BoardListPage = () => (
   <div>
     <NavBar />
+    <div className="flex justify-center py-24">
+      <div className="w-full max-w-5xl">
+        <div className="flex flex-wrap justify-center">
+          {[...Array(9)].map((_, i) => (
+            <BoardItem index={i} key={i} />
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
