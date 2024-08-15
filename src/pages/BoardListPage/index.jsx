3bc914c69ac0b4b0f3d5d8a8bbae3d024b568c83
Giorgi Zviadadze <giosofteng@gmail.com>
2024-08-14 20:15:34 -0400

    Update boards page styling

--- src/pages/BoardListPage/index.jsx
+++ src/pages/BoardListPage/index.jsx
@@ -1,10 +1,10 @@
-import { NavBar } from "shared-components/NavBar";
 import { BoardItem } from "./BoardItem";
+import { NavBar } from "shared-components/NavBar";
 
 export const BoardListPage = () => (
   <div>
     <NavBar />
-    <div className="flex justify-center py-24">
+    <div className="flex justify-center py-16">
       <div className="w-full max-w-5xl">
         <div className="flex flex-wrap justify-center">
           {[...Array(9)].map((_, i) => (
